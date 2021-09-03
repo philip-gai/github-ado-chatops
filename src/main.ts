139e72bc700546cca4b65c7fcf215ceea893611a
Philip Gai <philip-gai@github.com>
2021-09-03 21:27:49 +0000

    Fixed all errors when rewriting for Actions

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,6 @@ async function run(): Promise<void> {
   try {
     const context = github.context;
 
-    const ado_domain = core.getInput('ado_domain');
-    const ado_org = core.getInput('ado_org');
-    const ado_project = core.getInput('ado_project');
-    const ado_repo = core.getInput('ado_repo');
-    const ado_pat = core.getInput('ado_pat');
-
-    // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    core.debug(`ado_domain: ${ado_domain}!`);
-    core.debug(`ado_org: ${ado_org}!`);
-    core.debug(`ado_project: ${ado_project}!`);
-    core.debug(`ado_repo: ${ado_repo}!`);
-    core.debug(`ado_pat: ${ado_pat}!`);
-
     if (context.eventName === 'issue_comment.created') {
       const chatOpService = await ChatOpService.build(context);
       chatOpService.tryCreateBranch(context);
